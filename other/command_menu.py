2fe87ee58a92fa909bbbff74f63d54aa7c242e1e
Egor Fedchenko <egorcafev4enko@gmail.com>
2023-10-16 17:23:35 +0700

    refactor

--- other/command_menu.py
+++ other/command_menu.py
@@ -3,7 +3,5 @@ from aiogram import types
 
 async def set_default_commands(bot):
     await bot.set_my_commands([
-        types.BotCommand(command="start", description="Запустить бота"),
-        types.BotCommand(command="get_data", description="111"),
-        types.BotCommand(command="help", description="Помощь"),
+        types.BotCommand(command="start", description="Запустить бота")
     ])
